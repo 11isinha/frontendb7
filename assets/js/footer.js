2b111bd768a320360ac3ae0395468a5dd129a63f
isinha🫧 <isabella.r.rosa@aluno.senai.br>
2026-05-29 13:25:46 -0300

    Delete assets directory

--- assets/js/footer.js
+++
@@ -1,9 +0,0 @@
-let footerBtn = document.querySelector('[data-role="scroll-top"]');
-
-footerBtn.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
-    });
-});
